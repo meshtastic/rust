ccc47d5e0ef5674c0cf735d248114cff8c41b848
Brannon Dorsey <brannon@brannondorsey.com>
2025-06-21 18:24:00 -0400

    chore: Go back to original method of allowing clip lints in build.rs

--- build.rs
+++ build.rs
@@ -64,12 +64,10 @@ fn main() -> std::io::Result<()> {
     #[cfg(feature = "serde")]
     {
         config.type_attribute(".", "#[serde(rename_all = \"camelCase\")]");
+        config.type_attribute(".", "#[allow(clippy::doc_lazy_continuation)]");
+        config.type_attribute(".", "#[allow(clippy::empty_docs)]");
+        config.type_attribute(".", "#[allow(clippy::doc_overindented_list_items)]");
     }
-
-    config.type_attribute(".", "#[allow(clippy::doc_lazy_continuation)]");
-    config.type_attribute(".", "#[allow(clippy::empty_docs)]");
-    config.type_attribute(".", "#[allow(missing_docs)]");
-    config.type_attribute(".", "#[allow(clippy::doc_overindented_list_items)]");
 
     config.out_dir(gen_dir);
     config.compile_protos(&protos, &[protobufs_dir])
